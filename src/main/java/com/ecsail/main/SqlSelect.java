39c0f917bb4d6f8110ac1cc985e321af6a6075ef
Perry Cameron <pcameron@iu.edu>
2020-09-28 13:30:50 -0400

    Fixed bug where if a membership was not active but still paid some money for winter storage, they wouldn't be counted as memberships not yet renewed.

--- src/main/java/com/ecsail/main/SqlSelect.java
+++ src/main/java/com/ecsail/main/SqlSelect.java
@@ -871,9 +871,10 @@ public class SqlSelect {
 		ResultSet rs;
 		try {
 			Statement stmt = ConnectDatabase.connection.createStatement();
-			rs = stmt.executeQuery("SELECT (Select Count(MS_ID) from membership where "
-					+ "ACTIVE_MEMBERSHIP=true) - (Select Count(MONEY_ID) from money where "
-					+ "commited=true and fiscal_year='" + year + "') AS INCREASE;");
+			rs = stmt.executeQuery("SELECT (SELECT Count(MS_ID) from membership "
+					+ "where ACTIVE_MEMBERSHIP=true) - (SELECT Count(m.MS_ID) from membership "
+					+ "m right join money mo on mo.MS_ID=m.MS_ID where mo.commited=true and "
+					+ "mo.fiscal_year='2020' and m.ACTIVE_MEMBERSHIP=true) AS INCREASE;");
 			rs.next();
 			number = rs.getInt("INCREASE");
 		
