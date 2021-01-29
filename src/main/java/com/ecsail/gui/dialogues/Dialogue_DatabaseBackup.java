023189cbe52c13a9cb3943d2edfc3159d4880583
Perry Cameron <pcameron@iu.edu>
2021-01-28 22:28:04 -0500

    Added a grid pane and several columns.

--- src/main/java/com/ecsail/gui/dialogues/Dialogue_DatabaseBackup.java
+++ src/main/java/com/ecsail/gui/dialogues/Dialogue_DatabaseBackup.java
@@ -7,9 +7,11 @@ import com.ecsail.structures.Object_TupleCount;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -21,7 +23,7 @@ public class Dialogue_DatabaseBackup extends Stage {
 		VBox vboxGrey = new VBox(); // this is the vbox for organizing all the widgets
 		VBox vboxBlue = new VBox();
 		VBox vboxPink = new VBox(); // this creates a pink border around the table
-		Scene scene = new Scene(vboxBlue, 600, 300);
+		Scene scene = new Scene(vboxBlue, 400, 300);
 		
 		/////////////////// ATTRIBUTES ///////////////////
 		vboxBlue.setId("box-blue");
@@ -31,12 +33,10 @@ public class Dialogue_DatabaseBackup extends Stage {
 		// vboxGrey.setId("slip-box");
 		vboxGrey.setPrefHeight(688);
 		scene.getStylesheets().add("stylesheet.css");
-		setTitle("Window Stub");
+		setTitle("Database BackUp");
 		Image mainIcon = new Image(getClass().getResourceAsStream("/ECSC64.png"));
 		
 		//////////////// ADD CONTENT ///////////////////
-		
-		
 		
 		vboxGrey.getChildren().add(addInformationVBox());
 		vboxBlue.getChildren().add(vboxPink);
@@ -48,21 +48,230 @@ public class Dialogue_DatabaseBackup extends Stage {
 	
 	private VBox addInformationVBox() {
 		VBox iBox = new VBox();
-		iBox.getChildren().add(new Label("Database backed up"));
-		iBox.getChildren().addAll(new Text("Membership records: " + tuples.get(tuples.size() - 1).getMembershipSize()));
-		iBox.getChildren().addAll(new Text("Id records: " + tuples.get(tuples.size() - 1).getIdSize()));
-		iBox.getChildren().addAll(new Text("People records: " + tuples.get(tuples.size() - 1).getPeopleSize()));
-		iBox.getChildren().addAll(new Text("Phone records: " + tuples.get(tuples.size() - 1).getPhoneSize()));
-		iBox.getChildren().addAll(new Text("Boat records: " + tuples.get(tuples.size() - 1).getBoatSize()));
-		iBox.getChildren().addAll(new Text("Boat Owner records: " + tuples.get(tuples.size() - 1).getBoatOwnerSize()));
-		iBox.getChildren().addAll(new Text("Slip records: " + tuples.get(tuples.size() - 1).getSlipsSize()));
-		iBox.getChildren().addAll(new Text("Memo records: " + tuples.get(tuples.size() - 1).getMemosSize()));
-		iBox.getChildren().addAll(new Text("Email records: " + tuples.get(tuples.size() - 1).getEmailSize()));
-		iBox.getChildren().addAll(new Text("Money records: " + tuples.get(tuples.size() - 1).getMoniesSize()));
-		iBox.getChildren().addAll(new Text("Deposit records: " + tuples.get(tuples.size() - 1).getDepositsSize()));
-		iBox.getChildren().addAll(new Text("Payment records: " + tuples.get(tuples.size() - 1).getPaymentsSize()));
-		iBox.getChildren().addAll(new Text("Defined Fee records: " + tuples.get(tuples.size() - 1).getDefinedFeesSize()));
-		iBox.getChildren().addAll(new Text("Work Credit records: " + tuples.get(tuples.size() - 1).getWorkCreditsSize()));
+		GridPane pane = new GridPane();
+		int r = tuples.size() - 1;
+		Font font1 = Font.font("Verdana", FontWeight.BOLD, 14);
+		Text text;
+		pane.setHgap(25);
+		
+		iBox.getChildren().add(new Text("Database sucessfully backed up"));
+		
+		////////// HEADERS //////////////
+		text = new Text("Record Type");
+		text.setFont(font1);	
+		pane.add(text, 0, 0);
+		
+		text = new Text("New");
+		text.setFont(font1);	
+		pane.add(text, 1, 0);
+		
+		text = new Text("Modified");
+		text.setFont(font1);	
+		pane.add(text, 2, 0);
+		
+		text = new Text("Total");
+		text.setFont(font1);	
+		pane.add(text, 3, 0);
+		
+		////////// ROW 1 ////////////
+		
+		text = new Text("Membership");	
+		pane.add(text, 0, 1);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 1);
+		
+		text = new Text("0");
+		pane.add(text, 2, 1);
+		
+		text = new Text(tuples.get(r).getMembershipSize() + "");	
+		pane.add(text, 3, 1);
+		
+		////////// ROW 2 ////////////
+		
+		text = new Text("Id");	
+		pane.add(text, 0, 2);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 2);
+		
+		text = new Text("0");
+		pane.add(text, 2, 2);
+		
+		text = new Text(tuples.get(r).getIdSize() + "");	
+		pane.add(text, 3, 2);
+		
+		////////// ROW 3 ////////////
+		
+		text = new Text("People");	
+		pane.add(text, 0, 3);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 3);
+		
+		text = new Text("0");
+		pane.add(text, 2, 3);
+		
+		text = new Text(tuples.get(r).getPeopleSize() + "");	
+		pane.add(text, 3, 3);
+		
+		////////// ROW 4 ////////////
+		
+		text = new Text("Phone");	
+		pane.add(text, 0, 4);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 4);
+		
+		text = new Text("0");
+		pane.add(text, 2, 4);
+		
+		text = new Text(tuples.get(r).getPhoneSize() + "");	
+		pane.add(text, 3, 4);
+		
+		////////// ROW 5 ////////////
+		
+		text = new Text("Boat");	
+		pane.add(text, 0, 5);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 5);
+		
+		text = new Text("0");
+		pane.add(text, 2, 5);
+		
+		text = new Text(tuples.get(r).getBoatSize() + "");	
+		pane.add(text, 3, 5);
+		
+		////////// ROW 6 ////////////
+		
+		text = new Text("Boat Owner");	
+		pane.add(text, 0, 6);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 6);
+		
+		text = new Text("0");
+		pane.add(text, 2, 6);
+		
+		text = new Text(tuples.get(r).getBoatOwnerSize() + "");	
+		pane.add(text, 3, 6);
+		
+		////////// ROW 7 ////////////
+		
+		text = new Text("Slip");	
+		pane.add(text, 0, 7);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 7);
+		
+		text = new Text("0");
+		pane.add(text, 2, 7);
+		
+		text = new Text(tuples.get(r).getSlipsSize() + "");	
+		pane.add(text, 3, 7);
+		
+		////////// ROW 8 ////////////
+		
+		text = new Text("Memo");	
+		pane.add(text, 0, 8);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 8);
+		
+		text = new Text("0");
+		pane.add(text, 2, 8);
+		
+		text = new Text(tuples.get(r).getMemosSize() + "");	
+		pane.add(text, 3, 8);
+		
+		////////// ROW 9 ////////////
+		
+		text = new Text("Email");	
+		pane.add(text, 0, 9);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 9);
+		
+		text = new Text("0");
+		pane.add(text, 2, 9);
+		
+		text = new Text(tuples.get(r).getEmailSize() + "");	
+		pane.add(text, 3, 9);
+		
+		////////// ROW 10 ////////////
+		
+		text = new Text("Money");	
+		pane.add(text, 0, 10);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 10);
+		
+		text = new Text("0");
+		pane.add(text, 2, 10);
+		
+		text = new Text(tuples.get(r).getMoniesSize() + "");	
+		pane.add(text, 3, 10);
+		
+		////////// ROW 11 ////////////
+		
+		text = new Text("Deposit");	
+		pane.add(text, 0, 11);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 11);
+		
+		text = new Text("0");
+		pane.add(text, 2, 11);
+		
+		text = new Text(tuples.get(r).getDepositsSize() + "");	
+		pane.add(text, 3, 11);
+		
+		////////// ROW 12 ////////////
+		
+		text = new Text("Payment");	
+		pane.add(text, 0, 12);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 12);
+		
+		text = new Text("0");
+		pane.add(text, 2, 12);
+		
+		text = new Text(tuples.get(r).getPaymentsSize() + "");	
+		pane.add(text, 3, 12);
+		
+		////////// ROW 13 ////////////
+		
+		text = new Text("Defined Fee");	
+		pane.add(text, 0, 13);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 13);
+		
+		text = new Text("0");
+		pane.add(text, 2, 13);
+		
+		text = new Text(tuples.get(r).getDefinedFeesSize() + "");	
+		pane.add(text, 3, 13);
+		
+		////////// ROW 14 ////////////
+		
+		text = new Text("Work Credit");	
+		pane.add(text, 0, 14);
+		
+		text = new Text("0");	
+		pane.add(text, 1, 14);
+		
+		text = new Text("0");
+		pane.add(text, 2, 14);
+		
+		text = new Text(tuples.get(r).getWorkCreditsSize() + "");	
+		pane.add(text, 3, 14);
+		
+		iBox.getChildren().add(pane);
 		return iBox;
 	}
+	
+
 }
