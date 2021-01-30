9497fd2f80d61c1f8be11dc84cdfb28f1d869746
Perry Cameron <pcameron@iu.edu>
2021-01-29 20:17:30 -0500

    Added two fields catagory and money_id

--- src/main/java/com/ecsail/structures/Object_Memo.java
+++ src/main/java/com/ecsail/structures/Object_Memo.java
@@ -11,14 +11,18 @@ public class Object_Memo {
 	private IntegerProperty msid;
 	private StringProperty memo_date;
 	private StringProperty memo;
+	private IntegerProperty money_id;
+	private StringProperty catagory;
 	
 	public Object_Memo(Integer memo_id, Integer msid, String memo_date,
-			String memo) {
+			String memo, Integer money_id, String catagory) {
 		super();
 		this.memo_id = new SimpleIntegerProperty(memo_id);
 		this.msid = new SimpleIntegerProperty(msid);
 		this.memo_date = new SimpleStringProperty(memo_date);
 		this.memo = new SimpleStringProperty(memo);
+		this.money_id = new SimpleIntegerProperty(money_id);
+		this.catagory = new SimpleStringProperty(catagory);
 	}
 
 	public final IntegerProperty memo_idProperty() {
@@ -26,81 +30,88 @@ public class Object_Memo {
 	}
 	
 
-
-
 	public final int getMemo_id() {
 		return this.memo_idProperty().get();
 	}
 	
-
-
 
 	public final void setMemo_id(final int memo_id) {
 		this.memo_idProperty().set(memo_id);
 	}
 	
 
-
-
 	public final IntegerProperty msidProperty() {
 		return this.msid;
 	}
 	
-
-
 
 	public final int getMsid() {
 		return this.msidProperty().get();
 	}
 	
 
-
-
 	public final void setMsid(final int msid) {
 		this.msidProperty().set(msid);
 	}
 	
-
-
 
 	public final StringProperty memo_dateProperty() {
 		return this.memo_date;
 	}
 	
 
-
-
 	public final String getMemo_date() {
 		return this.memo_dateProperty().get();
 	}
 	
-
-
 
 	public final void setMemo_date(final String memo_date) {
 		this.memo_dateProperty().set(memo_date);
 	}
 	
 
-
-
 	public final StringProperty memoProperty() {
 		return this.memo;
 	}
 	
-
-
 
 	public final String getMemo() {
 		return this.memoProperty().get();
 	}
 	
 
-
-
 	public final void setMemo(final String memo) {
 		this.memoProperty().set(memo);
 	}
 	
+
+	public final IntegerProperty money_idProperty() {
+		return this.money_id;
+	}
 	
+
+	public final int getMoney_id() {
+		return this.money_idProperty().get();
+	}
+	
+
+	public final void setMoney_id(final int money_id) {
+		this.money_idProperty().set(money_id);
+	}
+	
+
+	public final StringProperty catagoryProperty() {
+		return this.catagory;
+	}
+	
+
+	public final String getCatagory() {
+		return this.catagoryProperty().get();
+	}
+	
+
+	public final void setCatagory(final String catagory) {
+		this.catagoryProperty().set(catagory);
+	}
+
 }
