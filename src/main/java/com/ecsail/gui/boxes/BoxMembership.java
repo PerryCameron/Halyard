97708654a5cc3c5b19c4ebcffe1f75541cc9e0b0
Perry Cameron <pcameron@iu.edu>
2021-02-23 21:16:26 -0500

    Added Record year to prevent confusion when looking at past years.

--- src/main/java/com/ecsail/gui/boxes/BoxMembership.java
+++ src/main/java/com/ecsail/gui/boxes/BoxMembership.java
@@ -24,18 +24,22 @@ public class BoxMembership extends HBox {
         HBox hbox2 = new HBox(); // sub HBox1 b
         HBox hbox3 = new HBox();
         HBox hbox4 = new HBox();
+        HBox hbox5 = new HBox();
 
+        labels.getSelectedYear().setText(membership.getSelectedYear());
         labels.getJoinDate().setText(membership.getJoinDate());
         labels.getMemberID().setText("" + membership.getMembershipId());
         labels.getMemberType().setText("" + MembershipType.getByCode(membership.getMemType()));
         labels.getStatus().setText(getStatus());
      
         ///////////// ATTRIBUTES ///////////
+        labels.getSelectedYear().setStyle("-fx-font-weight: bold;");
         labels.getJoinDate().setStyle("-fx-font-weight: bold;");
         labels.getMemberID().setStyle("-fx-font-weight: bold;");
         labels.getMemberType().setStyle("-fx-font-weight: bold;");
         labels.getStatus().setStyle("-fx-font-weight: bold;");
         
+        hbox5.getChildren().addAll(new Label("Record Year: "),labels.getSelectedYear());
         hbox1.getChildren().addAll(new Label("Membership ID: "),labels.getMemberID());
         hbox2.getChildren().addAll(new Label("Membership Type: "),labels.getMemberType());
         hbox3.getChildren().addAll(new Label("Join Date: "), labels.getJoinDate());
@@ -45,13 +49,15 @@ public class BoxMembership extends HBox {
         hbox2.setSpacing(5);
         hbox3.setSpacing(5);
         hbox4.setSpacing(5);
+        hbox5.setSpacing(5);
         
         hbox1.setAlignment(Pos.CENTER_LEFT);
         hbox2.setAlignment(Pos.CENTER_LEFT);
         hbox3.setAlignment(Pos.CENTER_LEFT);
         hbox4.setAlignment(Pos.CENTER_LEFT);
+        hbox5.setAlignment(Pos.CENTER_LEFT);
     
-        getChildren().addAll(hbox1,hbox2,hbox3,hbox4);
+        getChildren().addAll(hbox5,hbox1,hbox2,hbox3,hbox4);
         
 	} // CONSTRUCTOR END
 	
