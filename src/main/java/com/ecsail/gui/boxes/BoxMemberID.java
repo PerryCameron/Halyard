8eb4f4abf1a8b8ee51ba8024814874e2c6aed2a1
perry <pcameron28@yahoo.com>
2021-03-31 13:56:59 -0400

    Changed a couple anonmyous inner classes to lambda

--- src/main/java/com/ecsail/gui/boxes/BoxMemberID.java
+++ src/main/java/com/ecsail/gui/boxes/BoxMemberID.java
@@ -23,7 +23,6 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
