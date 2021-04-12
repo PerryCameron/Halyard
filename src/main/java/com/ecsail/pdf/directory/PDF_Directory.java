a77d4ed21501e8b50227c7467a856193a4023b06
perry <pcameron28@yahoo.com>
2021-04-12 13:34:06 -0400

    Moved members by number stuff into its own class.

--- src/main/java/com/ecsail/pdf/directory/PDF_Directory.java
+++ src/main/java/com/ecsail/pdf/directory/PDF_Directory.java
@@ -4,7 +4,6 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -15,16 +14,9 @@ import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.element.AreaBreak;
-import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.AreaBreakType;
-import com.itextpdf.layout.property.HorizontalAlignment;
-import com.itextpdf.layout.property.TextAlignment;
-import com.itextpdf.layout.property.VerticalAlignment;
-
 import javafx.collections.ObservableList;
 
 public class PDF_Directory {
@@ -86,86 +78,14 @@ public class PDF_Directory {
 		
 		doc.add(new PDF_ChapterPage(1, "Membership Information",set));
 		doc.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
-		createMemberInfoPages(doc);
 		
-		createMemberByNumberPages(doc);
+		createMemberInfoPages(doc);
+		doc.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+		
+		new PDF_MembersByNumber(set, doc, rosters);
 		doc.close();
 	}
-	
-	private void createMemberByNumberPages(Document doc) {
-		Collections.sort(this.rosters , Comparator.comparing(Object_MembershipList::getMembershipId));
-		ArrayList<Table> tables = new ArrayList<Table>();
-		
-		int count = 0;
-		int columnLength = 28;
-		int remainder = rosters.size() % columnLength;
-		int numberOfColumnTables = rosters.size() / columnLength;
-		if(remainder > 0) numberOfColumnTables++;
-		Table columnTable;
-		while(count < rosters.size() - remainder) {
-			columnTable = new Table(1);
-			count = fillColumnTable(columnLength,count,columnTable);
-			tables.add(columnTable);
-		}
-		/// creates a column table with the remainder
-		while(count < rosters.size()) {
-			columnTable = new Table(1);
-			count = fillColumnTable(remainder, count, columnTable);
-			tables.add(columnTable);
-		}
-		Table listTable = new Table(5);
-		listTable.setHorizontalAlignment(HorizontalAlignment.CENTER);
-		listTable.addCell(tables.get(0));
-		listTable.addCell(tables.get(1));
-		listTable.addCell(tables.get(2));
-		listTable.addCell(tables.get(3));
-		listTable.addCell(tables.get(4));
-		System.out.println("tables=" + numberOfColumnTables);
-		doc.add(listTable);
-		doc.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
-		listTable = new Table(5);
-		listTable.setHorizontalAlignment(HorizontalAlignment.CENTER);
-		listTable.addCell(tables.get(5));
-		listTable.addCell(tables.get(6));
-		listTable.addCell(tables.get(7));
-		listTable.addCell(tables.get(8));
-		listTable.addCell(tables.get(9));
-		doc.add(listTable);
-	}
-	
-	
-	private int fillColumnTable(int columnSize, int count, Table columnTable) {
-		for(int i = 0; i < columnSize; i++) {
-			columnTable.addCell(createMembershipEntry(rosters.get(count )));
-			count++;
-		}
-		return count;
-	}
-	
-	private Cell createMembershipEntry(Object_MembershipList m) {
-		Cell cell;
-		Paragraph p;
-		cell = new Cell();
-		cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
-		cell.setBorder(Border.NO_BORDER);
-		p = new Paragraph(m.getMembershipId() + "  " + m.getLname());
-		p.setFontSize(set.getNormalFontSize() - 2);
-		p.setTextAlignment(TextAlignment.LEFT);
-		cell.add(p);
-		return cell;
-	}
-	
-	private Cell addVerticalSpace(int space) {
-		String carrageReturn = "";
-		for(int i = 0; i < space; i++) {
-			carrageReturn += "\n";
-		}
-		Cell cell = new Cell();
-		cell.add(new Paragraph(carrageReturn));
-		cell.setBorder(Border.NO_BORDER);
-		return cell;
-	}
-	
+
 	private void createMemberInfoPages(Document doc) {
 			int count = 0;
 			doc.add(new Paragraph("\n"));
--- src/main/java/com/ecsail/pdf/directory/PDF_MembersByNumber.java
+++ src/main/java/com/ecsail/pdf/directory/PDF_MembersByNumber.java
@@ -1,13 +1,17 @@
 package com.ecsail.pdf.directory;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 import com.ecsail.structures.Object_MembershipList;
-import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.AreaBreakType;
 import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
@@ -16,34 +20,59 @@ import javafx.collections.ObservableList;
 
 public class PDF_MembersByNumber {
 	ObservableList<Object_MembershipList> rosters;
-	int startNumber = 0;
 	PDF_Object_Settings set;
-	public PDF_MembersByNumber(PDF_Object_Settings set) {
+	
+	public PDF_MembersByNumber(PDF_Object_Settings set, Document doc, ObservableList<Object_MembershipList> rosters) {
 		this.set = set;
 		this.rosters = rosters;
-		//setWidth(PageSize.A5.getWidth() * 0.9f);  // makes table 90% of page width
-		//setHorizontalAlignment(HorizontalAlignment.LEFT);
-		
-		// sort membership by membership number
 		Collections.sort(this.rosters , Comparator.comparing(Object_MembershipList::getMembershipId));
-		///////////////// Cells ////////////////////////////
-		Cell cell;
-		while(startNumber < rosters.size()) {
-			
-			cell = new Cell();
-			cell.add(createColumnTable(28));
-		}
-
-	}
-	
-	private Table createColumnTable(int columnSize) {
-		Table columnTable = new Table(1);
-		for(int i = 0; i < columnSize; i++) {
-			columnTable.addCell(createMembershipEntry(rosters.get(startNumber)));
-			startNumber++;
-		}
+		ArrayList<Table> tables = new ArrayList<Table>();
 		
-		return columnTable;
+		int count = 0;
+		int columnLength = 28;
+		int remainder = rosters.size() % columnLength;
+		int numberOfColumnTables = rosters.size() / columnLength;
+		if(remainder > 0) numberOfColumnTables++;
+		Table columnTable;
+		
+		while(count < rosters.size() - remainder) {
+			columnTable = new Table(1);
+			count = fillColumnTable(columnLength,count,columnTable);
+			tables.add(columnTable);
+		}
+		/// creates a column table with the remainder
+		while(count < rosters.size()) {
+			columnTable = new Table(1);
+			count = fillColumnTable(remainder, count, columnTable);
+			tables.add(columnTable);
+		}
+		Table listTable = new Table(5);
+		listTable.setHorizontalAlignment(HorizontalAlignment.CENTER);
+		listTable.addCell(tables.get(0));
+		listTable.addCell(tables.get(1));
+		listTable.addCell(tables.get(2));
+		listTable.addCell(tables.get(3));
+		listTable.addCell(tables.get(4));
+		System.out.println("tables=" + numberOfColumnTables);
+		doc.add(new Paragraph("\n"));
+		doc.add(listTable);
+		doc.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+		listTable = new Table(5);
+		listTable.setHorizontalAlignment(HorizontalAlignment.CENTER);
+		listTable.addCell(tables.get(5));
+		listTable.addCell(tables.get(6));
+		listTable.addCell(tables.get(7));
+		listTable.addCell(tables.get(8));
+		listTable.addCell(tables.get(9));
+		doc.add(new Paragraph("\n"));
+		doc.add(listTable);
+	}
+	private int fillColumnTable(int columnSize, int count, Table columnTable) {
+		for(int i = 0; i < columnSize; i++) {
+			columnTable.addCell(createMembershipEntry(rosters.get(count )));
+			count++;
+		}
+		return count;
 	}
 	
 	private Cell createMembershipEntry(Object_MembershipList m) {
@@ -51,12 +80,11 @@ public class PDF_MembersByNumber {
 		Paragraph p;
 		cell = new Cell();
 		cell.setVerticalAlignment(VerticalAlignment.MIDDLE);
-		p = new Paragraph(m.getMembershipId() + " " + m.getLname());
-		p.setFontSize(set.getNormalFontSize());
+		cell.setBorder(Border.NO_BORDER);
+		p = new Paragraph(m.getMembershipId() + "  " + m.getLname());
+		p.setFontSize(set.getNormalFontSize() - 2);
 		p.setTextAlignment(TextAlignment.LEFT);
 		cell.add(p);
 		return cell;
 	}
-	
-
 }
