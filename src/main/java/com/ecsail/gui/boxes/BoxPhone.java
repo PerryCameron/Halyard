3064ba78ac113a7db9dfd952b997d820cb565957
PerryCameron <pcameron28@yahoo.com>
2021-08-19 19:18:38 -0400

    added error checking for phone field

--- src/main/java/com/ecsail/gui/boxes/BoxPhone.java
+++ src/main/java/com/ecsail/gui/boxes/BoxPhone.java
@@ -2,6 +2,7 @@ package com.ecsail.gui.boxes;
 
 import java.util.Arrays;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 import com.ecsail.enums.PhoneType;
 import com.ecsail.main.EditCell;
@@ -38,88 +39,141 @@ import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
 public class BoxPhone extends HBox {
-	
-	private Object_Person person;
-	private TableView<Object_Phone> phoneTableView;
-	private ObservableList<Object_Phone> phone;
-	
-	public BoxPhone(Object_Person p) {
-		this.person = p;  // the below callback is to allow commit when focus removed, overrides FX default behavior
-		this.phone = FXCollections.observableArrayList(new Callback<Object_Phone, Observable[]>() {
-			@Override
-			public Observable[] call(Object_Phone param) {
-				return new Observable[] { param.isListedProperty() };
+    
+    private Object_Person person;
+    private TableView<Object_Phone> phoneTableView;
+    private ObservableList<Object_Phone> phone;
+    
+    public BoxPhone(Object_Person p) {
+        this.person = p;  // the below callback is to allow commit when focus removed, overrides FX default behavior
+        this.phone = FXCollections.observableArrayList(new Callback<Object_Phone, Observable[]>() {
+            @Override
+            public Observable[] call(Object_Phone param) {
+                return new Observable[] { param.isListedProperty() };
 
-			}
-		});
-		this.phone.addAll(SqlSelect.getPhone(person.getP_id()));
-		
-		/////// OBJECT INSTANCE //////	
-		VBox vboxButtons = new VBox(); // holds phone buttons
-		Button phoneAdd = new Button("Add");
-		Button phoneDelete = new Button("Delete");
-		HBox hboxGrey = new HBox(); // this is here for the grey background to make nice apperence
-		VBox vboxPink = new VBox(); // this creates a pink border around the table
-		phoneTableView = new TableView<Object_Phone>();
+            }
+        });
+        this.phone.addAll(SqlSelect.getPhone(person.getP_id()));
+        
+        /////// OBJECT INSTANCE //////  
+        VBox vboxButtons = new VBox(); // holds phone buttons
+        Button phoneAdd = new Button("Add");
+        Button phoneDelete = new Button("Delete");
+        HBox hboxGrey = new HBox(); // this is here for the grey background to make nice apperence
+        VBox vboxPink = new VBox(); // this creates a pink border around the table
+        phoneTableView = new TableView<Object_Phone>();
 
-		//// OBJECT ATTRIBUTES /////
-		phoneAdd.setPrefWidth(60);
-		phoneDelete.setPrefWidth(60);
-		vboxButtons.setPrefWidth(80);
-		vboxButtons.setSpacing(5); // spacing between buttons
-		//hboxGrey.setPrefWidth(480);
-		
-		HBox.setHgrow(hboxGrey, Priority.ALWAYS);
-		HBox.setHgrow(vboxPink, Priority.ALWAYS);
-		HBox.setHgrow(phoneTableView, Priority.ALWAYS);
-		VBox.setVgrow(phoneTableView, Priority.ALWAYS);
-		
-		hboxGrey.setSpacing(10);  // spacing in between table and buttons
-		hboxGrey.setId("box-grey");
-		vboxPink.setId("box-pink");
-		hboxGrey.setPadding(new Insets(5,5,5,5));  // spacing around table and buttons
-		vboxPink.setPadding(new Insets(2,2,2,2)); // spacing to make pink fram around table
-		VBox.setVgrow(phoneTableView, Priority.ALWAYS);
+        //// OBJECT ATTRIBUTES /////
+        phoneAdd.setPrefWidth(60);
+        phoneDelete.setPrefWidth(60);
+        vboxButtons.setPrefWidth(80);
+        vboxButtons.setSpacing(5); // spacing between buttons
+        //hboxGrey.setPrefWidth(480);
+        
+        HBox.setHgrow(hboxGrey, Priority.ALWAYS);
+        HBox.setHgrow(vboxPink, Priority.ALWAYS);
+        HBox.setHgrow(phoneTableView, Priority.ALWAYS);
+        VBox.setVgrow(phoneTableView, Priority.ALWAYS);
+        
+        hboxGrey.setSpacing(10);  // spacing in between table and buttons
+        hboxGrey.setId("box-grey");
+        vboxPink.setId("box-pink");
+        hboxGrey.setPadding(new Insets(5,5,5,5));  // spacing around table and buttons
+        vboxPink.setPadding(new Insets(2,2,2,2)); // spacing to make pink fram around table
+        VBox.setVgrow(phoneTableView, Priority.ALWAYS);
 
-		///// TABLEVIE INSTANCE CREATION AND ATTRIBUTES /////
-		
-		phoneTableView.setItems(phone);
-		phoneTableView.setFixedCellSize(30);
-		phoneTableView.setEditable(true);
-		phoneTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY );
-			
-		// example for this column found at https://gist.github.com/james-d/be5bbd6255a4640a5357#file-editcell-java-L109
-		TableColumn<Object_Phone, String> Col1 = createColumn("Phone", Object_Phone::phoneNumberProperty);
+        ///// TABLEVIE INSTANCE CREATION AND ATTRIBUTES /////
+        
+        phoneTableView.setItems(phone);
+        phoneTableView.setFixedCellSize(30);
+        phoneTableView.setEditable(true);
+        phoneTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY );
+            
+        // example for this column found at https://gist.github.com/james-d/be5bbd6255a4640a5357#file-editcell-java-L109
+        TableColumn<Object_Phone, String> Col1 = createColumn("Phone", Object_Phone::phoneNumberProperty);
         Col1.setOnEditCommit(
                 new EventHandler<CellEditEvent<Object_Phone, String>>() {
                     @Override
                     public void handle(CellEditEvent<Object_Phone, String> t) {
+                        
                         ((Object_Phone) t.getTableView().getItems().get(
                                 t.getTablePosition().getRow())
                                 ).setPhoneNumber(t.getNewValue());
+                        String processedNumber = processNumber(t.getNewValue());        
                         int phone_id = ((Object_Phone) t.getTableView().getItems().get(t.getTablePosition().getRow())).getPhone_ID();
-                        	SqlUpdate.updatePhone("phone", phone_id, t.getNewValue());
+                            SqlUpdate.updatePhone("phone", phone_id, processedNumber);
+                        // update the observable list so that JavaFX will update the table cell
+                        for(Object_Phone p: phone) {
+                            if(p.getPhone_ID() == phone_id) p.setPhoneNumber(processedNumber);
+                        }
+                    }
+
+                    private String processNumber(String newValue) {
+                        // adds dashes
+                        if(Pattern.matches("\\d{10}", newValue)) {
+                            System.out.println("Added dashes");
+                            return addDashes(newValue);
+                        }
+                        // need to add area code
+                        else if(Pattern.matches("\\d{7}", newValue)) {
+                            System.out.println("Found 7 numbers, adding default area code");
+                            return addDashes("317" + newValue);
+                        }
+                        // perfect no need to change anything
+                        else if(Pattern.matches("(?:\\d{3}-){2}\\d{4}", newValue)) {
+                            return newValue;
+                        }
+                        // removes all junk as long as there are 10 numbers
+                        else if(keepOnlyNumbers(newValue).length() == 10) {
+                            System.out.println("Bad Formatting, fixing...");
+                            return addDashes(keepOnlyNumbers(newValue));
+                        }
+                        // removes all junk and adds default area code if there are 7 numbers
+                        else if(keepOnlyNumbers(newValue).length() == 7) {
+                            System.out.println("Bad Formatting, fixing...");
+                            return addDashes("317" + keepOnlyNumbers(newValue));
+                        }
+                        else {
+                            System.out.println("This is a bad number");
+                            return "illformatted number";
+                        }
+                    }
+
+                    private String addDashes(String newValue) {
+                        char charArray[] = newValue.toCharArray();
+                        String phoneNumber = "";
+                        for(int i = 0; i < charArray.length; i++) {
+                            if(i==3) phoneNumber += "-";
+                            phoneNumber += charArray[i];
+                            if(i==5) phoneNumber += "-";
+                        }
+                        return phoneNumber;
+                    }
+
+                    private String keepOnlyNumbers(String newValue) {
+                        String phoneNumber = newValue.replaceAll("[^0-9]", "");
+                        return phoneNumber;
                     }
                 }
             );
-		
+        
         // example for this column found at https://o7planning.org/en/11079/javafx-tableview-tutorial
         ObservableList<PhoneType> phoneTypeList = FXCollections.observableArrayList(PhoneType.values());
-		TableColumn<Object_Phone, PhoneType> Col2 = new TableColumn<Object_Phone, PhoneType>("Type");
-		Col2.setCellValueFactory(new Callback<CellDataFeatures<Object_Phone, PhoneType>, ObservableValue<PhoneType>>() {
-        	 
+        TableColumn<Object_Phone, PhoneType> Col2 = new TableColumn<Object_Phone, PhoneType>("Type");
+        Col2.setCellValueFactory(new Callback<CellDataFeatures<Object_Phone, PhoneType>, ObservableValue<PhoneType>>() {
+             
             @Override
             public ObservableValue<PhoneType> call(CellDataFeatures<Object_Phone, PhoneType> param) {
-            	Object_Phone thisPhone = param.getValue();
+                Object_Phone thisPhone = param.getValue();
                 String phoneCode = thisPhone.getPhoneType();
                 PhoneType phoneType = PhoneType.getByCode(phoneCode);
                 return new SimpleObjectProperty<PhoneType>(phoneType);
             }
         });
  
-		Col2.setCellFactory(ComboBoxTableCell.forTableColumn(phoneTypeList));
+        Col2.setCellFactory(ComboBoxTableCell.forTableColumn(phoneTypeList));
  
-		Col2.setOnEditCommit((CellEditEvent<Object_Phone, PhoneType> event) -> {
+        Col2.setOnEditCommit((CellEditEvent<Object_Phone, PhoneType> event) -> {
             TablePosition<Object_Phone, PhoneType> pos = event.getTablePosition();
             PhoneType newPhoneType = event.getNewValue();
             int row = pos.getRow();
@@ -127,72 +181,72 @@ public class BoxPhone extends HBox {
             SqlUpdate.updatePhone("phone_type", thisPhone.getPhone_ID(), newPhoneType.getCode());
             thisPhone.setPhoneType(newPhoneType.getCode());
         });
-		
-		// example for this column found at https://o7planning.org/en/11079/javafx-tableview-tutorial
-		TableColumn<Object_Phone, Boolean> Col3 = new TableColumn<Object_Phone, Boolean>("Listed");
-		Col3.setCellValueFactory(new Callback<CellDataFeatures<Object_Phone, Boolean>, ObservableValue<Boolean>>() {
-	            @Override
-	            public ObservableValue<Boolean> call(CellDataFeatures<Object_Phone, Boolean> param) {
-	            	Object_Phone phone = param.getValue();
-	                SimpleBooleanProperty booleanProp = new SimpleBooleanProperty(phone.isIsListed());
-	                // Note: singleCol.setOnEditCommit(): Not work for
-	                // CheckBoxTableCell.
-	                // When "isListed?" column change.
-	                booleanProp.addListener(new ChangeListener<Boolean>() {
-	                    @Override
-	                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue,
-	                            Boolean newValue) {
-	                        phone.setIsListed(newValue);
-	                        SqlUpdate.updateListed("phone_listed",phone.getPhone_ID(), newValue);
-	                    }
-	                });
-	                return booleanProp;
-	            }
-	        });
-	 
-		Col3.setCellFactory(new Callback<TableColumn<Object_Phone, Boolean>, //
-	        TableCell<Object_Phone, Boolean>>() {
-	            @Override
-	            public TableCell<Object_Phone, Boolean> call(TableColumn<Object_Phone, Boolean> p) {
-	                CheckBoxTableCell<Object_Phone, Boolean> cell = new CheckBoxTableCell<Object_Phone, Boolean>();
-	                cell.setAlignment(Pos.CENTER);
-	                return cell;
-	            }
-	        });
-		
-		/// sets width of columns by percentage
-		Col1.setMaxWidth( 1f * Integer.MAX_VALUE * 50);   // Phone
-		Col2.setMaxWidth( 1f * Integer.MAX_VALUE * 30 );  // Type
-		Col3.setMaxWidth( 1f * Integer.MAX_VALUE * 20 );  // Listed
+        
+        // example for this column found at https://o7planning.org/en/11079/javafx-tableview-tutorial
+        TableColumn<Object_Phone, Boolean> Col3 = new TableColumn<Object_Phone, Boolean>("Listed");
+        Col3.setCellValueFactory(new Callback<CellDataFeatures<Object_Phone, Boolean>, ObservableValue<Boolean>>() {
+                @Override
+                public ObservableValue<Boolean> call(CellDataFeatures<Object_Phone, Boolean> param) {
+                    Object_Phone phone = param.getValue();
+                    SimpleBooleanProperty booleanProp = new SimpleBooleanProperty(phone.isIsListed());
+                    // Note: singleCol.setOnEditCommit(): Not work for
+                    // CheckBoxTableCell.
+                    // When "isListed?" column change.
+                    booleanProp.addListener(new ChangeListener<Boolean>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue,
+                                Boolean newValue) {
+                            phone.setIsListed(newValue);
+                            SqlUpdate.updateListed("phone_listed",phone.getPhone_ID(), newValue);
+                        }
+                    });
+                    return booleanProp;
+                }
+            });
+     
+        Col3.setCellFactory(new Callback<TableColumn<Object_Phone, Boolean>, //
+            TableCell<Object_Phone, Boolean>>() {
+                @Override
+                public TableCell<Object_Phone, Boolean> call(TableColumn<Object_Phone, Boolean> p) {
+                    CheckBoxTableCell<Object_Phone, Boolean> cell = new CheckBoxTableCell<Object_Phone, Boolean>();
+                    cell.setAlignment(Pos.CENTER);
+                    return cell;
+                }
+            });
+        
+        /// sets width of columns by percentage
+        Col1.setMaxWidth( 1f * Integer.MAX_VALUE * 50);   // Phone
+        Col2.setMaxWidth( 1f * Integer.MAX_VALUE * 30 );  // Type
+        Col3.setMaxWidth( 1f * Integer.MAX_VALUE * 20 );  // Listed
 
-		
-		/////////////////// LISTENERS //////////////////////////////
-		
-		phoneAdd.setOnAction((event) -> {
-				int phone_id = SqlSelect.getCount("phone", "phone_id"); // get last phone_id number
-				phone_id++; // increase to first available number
-				if (SqlInsert.addRecord(phone_id, person.getP_id(), true, "new phone", "")) // if added with no errors
-					phone.add(new Object_Phone(phone_id, person.getP_id(), true, "new phone", "")); // lets add it to our GUI
-			});
+        
+        /////////////////// LISTENERS //////////////////////////////
+        
+        phoneAdd.setOnAction((event) -> {
+                int phone_id = SqlSelect.getCount("phone", "phone_id"); // get last phone_id number
+                phone_id++; // increase to first available number
+                if (SqlInsert.addRecord(phone_id, person.getP_id(), true, "new phone", "")) // if added with no errors
+                    phone.add(new Object_Phone(phone_id, person.getP_id(), true, "new phone", "")); // lets add it to our GUI
+            });
         
         phoneDelete.setOnAction((event) -> {
-            	int selectedIndex = phoneTableView.getSelectionModel().getSelectedIndex();
-            		if(selectedIndex >= 0)
-            			if(SqlDelete.deletePhone(phone.get(selectedIndex)))  // if it is properly deleted in our database
-            				phoneTableView.getItems().remove(selectedIndex); // remove it from our GUI
+                int selectedIndex = phoneTableView.getSelectionModel().getSelectedIndex();
+                    if(selectedIndex >= 0)
+                        if(SqlDelete.deletePhone(phone.get(selectedIndex)))  // if it is properly deleted in our database
+                            phoneTableView.getItems().remove(selectedIndex); // remove it from our GUI
         });
-		
-		///////////////////  SET CONTENT  ///////////////////////
-		
-		phoneTableView.getColumns().addAll(Arrays.asList(Col1,Col2,Col3));
-		vboxPink.getChildren().add(phoneTableView);  // adds pink border around table
-		vboxButtons.getChildren().addAll(phoneAdd, phoneDelete); // lines buttons up vertically
-		hboxGrey.getChildren().addAll(vboxPink,vboxButtons);
-		getChildren().add(hboxGrey);
-		
-	}  // end of constructor
-	
-	////////////////////////  CLASS METHODS //////////////////////////
+        
+        ///////////////////  SET CONTENT  ///////////////////////
+        
+        phoneTableView.getColumns().addAll(Arrays.asList(Col1,Col2,Col3));
+        vboxPink.getChildren().add(phoneTableView);  // adds pink border around table
+        vboxButtons.getChildren().addAll(phoneAdd, phoneDelete); // lines buttons up vertically
+        hboxGrey.getChildren().addAll(vboxPink,vboxButtons);
+        getChildren().add(hboxGrey);
+        
+    }  // end of constructor
+    
+    ////////////////////////  CLASS METHODS //////////////////////////
     private <T> TableColumn<T, String> createColumn(String title, Function<T, StringProperty> property) {
         TableColumn<T, String> col = new TableColumn<>(title);
         col.setCellValueFactory(cellData -> property.apply(cellData.getValue()));
