3debaff9d9692a0343fa824a690f5260274f0b48
PerryCameron <pcameron28@yahoo.com>
2022-02-08 10:35:04 -0500

    optimized class with lambdas and other fixes

--- src/main/java/com/ecsail/gui/boxes/HBoxOfficer.java
+++ src/main/java/com/ecsail/gui/boxes/HBoxOfficer.java
@@ -1,47 +1,40 @@
 package com.ecsail.gui.boxes;
 
-import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.function.Function;
-
 import com.ecsail.enums.Officer;
 import com.ecsail.main.EditCell;
 import com.ecsail.sql.SqlDelete;
 import com.ecsail.sql.SqlInsert;
+import com.ecsail.sql.SqlUpdate;
 import com.ecsail.sql.select.SqlOfficer;
 import com.ecsail.sql.select.SqlSelect;
-import com.ecsail.sql.SqlUpdate;
 import com.ecsail.structures.OfficerDTO;
 import com.ecsail.structures.PersonDTO;
-
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TablePosition;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.util.Callback;
+
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.function.Function;
 
 public class HBoxOfficer extends HBox {
 
-	private PersonDTO person;
-	private ObservableList<OfficerDTO> officer;
-	private TableView<OfficerDTO> officerTableView;
-	private String currentYear;
+	private final PersonDTO person;
+	private final ObservableList<OfficerDTO> officer;
+	private final TableView<OfficerDTO> officerTableView;
+	private final String currentYear;
 	
 	@SuppressWarnings("unchecked")
 	public HBoxOfficer(PersonDTO p) {
@@ -53,9 +46,9 @@ public class HBoxOfficer extends HBox {
 		Button officerAdd = new Button("Add");
 		Button officerDelete = new Button("Delete");
 		VBox vboxButton = new VBox(); // holds officer buttons
-		HBox hboxGrey = new HBox(); // this is here for the grey background to make nice apperence
+		HBox hboxGrey = new HBox(); // this is here for the grey background to make nice appearance
 		VBox vboxPink = new VBox(); // this creates a pink border around the table
-		officerTableView = new TableView<OfficerDTO>();
+		officerTableView = new TableView<>();
 
 		/////////////////  ATTRIBUTES  /////////////////////
 		officerAdd.setPrefWidth(60);
@@ -72,7 +65,7 @@ public class HBoxOfficer extends HBox {
 		hboxGrey.setId("box-grey");
 		vboxPink.setId("box-pink");
 		hboxGrey.setPadding(new Insets(5,5,5,5));  // spacing around table and buttons
-		vboxPink.setPadding(new Insets(2,2,2,2)); // spacing to make pink fram around table
+		vboxPink.setPadding(new Insets(2,2,2,2)); // spacing to make pink frame around table
 		
 		this.setId("box-blue");
 		
@@ -86,29 +79,22 @@ public class HBoxOfficer extends HBox {
 			TableColumn<OfficerDTO, String> Col1 = createColumn("Year", OfficerDTO::fiscal_yearProperty);
 			Col1.setSortType(TableColumn.SortType.DESCENDING);
 	        Col1.setOnEditCommit(
-	                new EventHandler<CellEditEvent<OfficerDTO, String>>() {
-	                    @Override
-	                    public void handle(CellEditEvent<OfficerDTO, String> t) {
-	                        ((OfficerDTO) t.getTableView().getItems().get(
-	                                t.getTablePosition().getRow())
-	                                ).setFiscal_year(t.getNewValue());
-	                        SqlUpdate.updateOfficer("off_year",t.getRowValue().getOfficer_id(), t.getNewValue());  // have to get by money id and pid eventually
-	                    }
-	                }
-	            );
+					t -> {
+						t.getTableView().getItems().get(
+								t.getTablePosition().getRow()).setFiscal_year(t.getNewValue());
+						SqlUpdate.updateOfficer("off_year",t.getRowValue().getOfficer_id(), t.getNewValue());  // have to get by money id and pid eventually
+					}
+			);
 	        
 	        
 	        ObservableList<Officer> officerList = FXCollections.observableArrayList(Officer.values());
-	    	final TableColumn<OfficerDTO, Officer> Col2 = new TableColumn<OfficerDTO, Officer>("Officers, Chairs and Board");
-	        Col2.setCellValueFactory(new Callback<CellDataFeatures<OfficerDTO, Officer>, ObservableValue<Officer>>() {
-	            @Override
-	            public ObservableValue<Officer> call(CellDataFeatures<OfficerDTO, Officer> param) {
-	                OfficerDTO thisofficer = param.getValue();
-	                String officerCode = thisofficer.getOfficer_type();
-	                Officer type = Officer.getByCode(officerCode);
-	                return new SimpleObjectProperty<Officer>(type);
-	            }
-	        });
+	    	final TableColumn<OfficerDTO, Officer> Col2 = new TableColumn<>("Officers, Chairs and Board");
+	        Col2.setCellValueFactory(param -> {
+				OfficerDTO thisOfficer = param.getValue();
+				String officerCode = thisOfficer.getOfficer_type();
+				Officer type = Officer.getByCode(officerCode);
+				return new SimpleObjectProperty<>(type);
+			});
 	        
 	        Col2.setCellFactory(ComboBoxTableCell.forTableColumn(officerList));
 	 
@@ -123,16 +109,12 @@ public class HBoxOfficer extends HBox {
 	        
 			TableColumn<OfficerDTO, String> Col3 = createColumn("Exp", OfficerDTO::board_yearProperty);
 	        Col3.setOnEditCommit(
-	                new EventHandler<CellEditEvent<OfficerDTO, String>>() {
-	                    @Override
-	                    public void handle(CellEditEvent<OfficerDTO, String> t) {
-	                        ((OfficerDTO) t.getTableView().getItems().get(
-	                                t.getTablePosition().getRow())
-	                                ).setBoard_year(t.getNewValue());  // need to change
-	                        	SqlUpdate.updateOfficer("board_year",t.getRowValue().getOfficer_id(), t.getNewValue());  // have to get by money id and pid eventually
-	                    }
-	                }
-	            );
+					t -> {
+						t.getTableView().getItems().get(
+								t.getTablePosition().getRow()).setBoard_year(t.getNewValue());  // need to change
+							SqlUpdate.updateOfficer("board_year",t.getRowValue().getOfficer_id(), t.getNewValue());  // have to get by money id and pid eventually
+					}
+			);
 	        
 			/// sets width of columns by percentage
 			Col1.setMaxWidth( 1f * Integer.MAX_VALUE * 20);   // Phone
@@ -140,31 +122,27 @@ public class HBoxOfficer extends HBox {
 			Col3.setMaxWidth( 1f * Integer.MAX_VALUE * 20 );  // Listed
 	        ////////////////////// LISTENERS /////////////////////////
 	    	    
-	        officerAdd.setOnAction(new EventHandler<ActionEvent>() {
-	            @Override public void handle(ActionEvent e) {
-					// get next available primary key for officer table
-	            	int officer_id = SqlSelect.getNextAvailablePrimaryKey("officer","o_id") + 1; // gets last memo_id number
-					// insert a new officer row into SQL and return true on success
-					if(SqlInsert.addOfficerRecord(officer_id,person.getP_id(),"0","new officer",Integer.parseInt(currentYear)))
-						// add a new row to the tableView to match new SQL entry
-	            		officer.add(new OfficerDTO(officer_id,person.getP_id(),"0","new officer",currentYear));
-					// Now we will sort it to the top
-					Collections.sort(officer, Comparator.comparing(OfficerDTO::getOfficer_id).reversed());
-					// this line prevents strange buggy behaviour
-					officerTableView.layout();
-					// edit the phone number cell after creating
-					officerTableView.edit(0, Col1);
-	            }
-	        });
+	        officerAdd.setOnAction(e -> {
+				// get next available primary key for officer table
+				int officer_id = SqlSelect.getNextAvailablePrimaryKey("officer","o_id") + 1; // gets last memo_id number
+				// insert a new officer row into SQL and return true on success
+				if(SqlInsert.addOfficerRecord(officer_id,person.getP_id(),"0","new officer",Integer.parseInt(currentYear)))
+					// add a new row to the tableView to match new SQL entry
+					officer.add(new OfficerDTO(officer_id,person.getP_id(),"0","new officer",currentYear));
+				// Now we will sort it to the top
+				officer.sort(Comparator.comparing(OfficerDTO::getOfficer_id).reversed());
+				// this line prevents strange buggy behaviour
+				officerTableView.layout();
+				// edit the phone number cell after creating
+				officerTableView.edit(0, Col1);
+			});
 	        
-	        officerDelete.setOnAction(new EventHandler<ActionEvent>() {
-	            @Override public void handle(ActionEvent e) {
-	            	int selectedIndex = officerTableView.getSelectionModel().getSelectedIndex();
-	            	if(selectedIndex >= 0)
-						if(SqlDelete.deleteOfficer(officer.get(selectedIndex)))
-							officerTableView.getItems().remove(selectedIndex);
-	            }
-	        });
+	        officerDelete.setOnAction(e -> {
+				int selectedIndex = officerTableView.getSelectionModel().getSelectedIndex();
+				if(selectedIndex >= 0)
+					if(SqlDelete.deleteOfficer(officer.get(selectedIndex)))
+						officerTableView.getItems().remove(selectedIndex);
+			});
 	        
 	        /////////////////// SET CONTENT //////////////////
 	        
