758949903b8affd54610f76ccfa7d81bfc6539a2
PerryCameron <pcameron28@yahoo.com>
2022-06-28 23:07:07 -0400

    updated SqlApi_key to use method executeSelectQuery, this will allow checks for ssh connection

--- src/main/java/com/ecsail/sql/select/SqlApi_key.java
+++ src/main/java/com/ecsail/sql/select/SqlApi_key.java
@@ -4,25 +4,23 @@ import com.ecsail.gui.dialogues.Dialogue_ErrorSQL;
 import com.ecsail.main.ConnectDatabase;
 import com.ecsail.main.Halyard;
 import com.ecsail.structures.ApiKeyDTO;
-import com.ecsail.structures.PersonDTO;
-import com.ecsail.structures.PhoneDTO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 public class SqlApi_key {
     public static ApiKeyDTO getApiKeyByName(String name) {  // if p_id = 0 then select all
         ApiKeyDTO thisApi = null;
+        String query = "select * from api_key  where NAME='" + name + "'";
         try {
             Statement stmt = ConnectDatabase.sqlConnection.createStatement();
-            ResultSet rs;
-            rs = stmt.executeQuery(Halyard.console.setRegexColor("select * from api_key  where NAME='" + name + "'"));
+            ResultSet rs = Halyard.getConnect().executeSelectQuery(stmt,query);
             while (rs.next()) {
                 thisApi = new ApiKeyDTO(rs.getInt("API_ID"), rs.getString("NAME"), rs.getString("APIKEY"),
                         rs.getString("ts"));
             }
+            stmt.close();
         } catch (SQLException e) {
             new Dialogue_ErrorSQL(e,"Unable to retrieve information","See below for details");
         }
